173ee5c5b766c4109886a67403c0ff1b9f1ec25a
kaungsike <kaungsike9@gmail.com>
2024-06-04 23:53:01 +0630

    Invoice App UI Finished By Using RibbleUI

--- _InvoiceApp/tailwind.config.js
+++ _InvoiceApp/tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily : {
+      sans : ["Nunito Sans", "sans-serif"]
+    },
     extend: {},
   },
   plugins: [require("rippleui")]
