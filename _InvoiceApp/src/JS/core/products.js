1c79f0d1d92d7e8c50ee591ad47c99644cbb0276
kaungsike <kaungsike9@gmail.com>
2024-06-08 03:15:01 -0700

    Code Update

--- _InvoiceApp/src/JS/core/products.js
+++ _InvoiceApp/src/JS/core/products.js
@@ -1,15 +1,21 @@
-import { productSelect } from "./selectors.js";
-
-// export const createProducts = (product) => {
-//     const option = document.createElement("option");
-//     option.innerText = product.name;
-//     option.value = product.id;
-
-//     return option;
-// }
+import { productGroup, productSelect, productTemplate } from "./selectors.js";
 
 export const productRender = (products) => {
     products.forEach((product) => {
         productSelect.append(new Option(product.name,product.id))
+    })
+}
+
+export const createProduct = (product) => {
+    const template = productTemplate.content.cloneNode(true);
+    template.querySelector(".product-name").innerText = product.name;
+    template.querySelector(".product-price").innerText = product.price;
+
+    return template;
+}
+
+export const createProductRender = (products) => {
+    products.forEach((product) => {
+        productGroup.append(createProduct(product))
     })
 }
--- _InvoiceApp/src/JS/core/selectors.js
+++ _InvoiceApp/src/JS/core/selectors.js
@@ -2,7 +2,9 @@ export const productSelect = document.querySelector("#productSelect");
 export const createForm = document.querySelector("#createForm");
 
 export const rowTemplate = document.querySelector("#rowTemplate");
+export const productTemplate = document.querySelector("#productTemplate")
 
 export const rowGroup = document.querySelector("#rowGroup");
+export const productGroup = document.querySelector("#productGroup")
 
 export const totalCost = document.querySelector("#totalCost")
