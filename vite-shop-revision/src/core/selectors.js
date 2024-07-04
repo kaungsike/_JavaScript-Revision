91e197a5af00589b4f123f8b906c38e97113d846
kaungsike <kaungsike9@gmail.com>
2024-07-04 19:43:20 +0630

    code update

--- vite-shop-revision/src/core/selectors.js
+++ vite-shop-revision/src/core/selectors.js
@@ -5,9 +5,11 @@ export const app = document.querySelector("#app");
 export const categoryTemplate = document.querySelector("#categoryTemplate");
 export const productTemplate = document.querySelector("#productTemplate");
 export const cartItemTemplate = document.querySelector("#cartItemTemplate");
+export const starTemplate = document.querySelector("#starTemplate");
 
 
 // groups
 export const categoryGroup = document.querySelector("#category-group");
 export const productGroup = document.querySelector("#product-group");
 export const cartItemGroup = document.querySelector("#cartItemGroup");
+export const productStarGroup = document.querySelector("#product-star");
